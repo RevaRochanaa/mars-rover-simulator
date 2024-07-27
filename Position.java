782b000456a7c90572bfb142ac80a1e85db3b250
RevaRochanaa <120038566+RevaRochanaa@users.noreply.github.com>
2024-07-28 00:20:06 +0530

    Update Position.java

--- Position.java
+++ Position.java
@@ -7,5 +7,29 @@ public class Position {
         this.y = y;
     }
 
-    // Getters, setters, and toString method
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x && y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
 }
